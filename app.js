3e75947007a4e0eb1d4570c5fdce860cd9be66e7
Sahil Bhanderi <sahilbhanderi02@gmail.com>
2026-05-27 18:13:50 +0530

    prettier fixed

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  
-  console.log('purposefully to fail the test')
-  let x= 75;
-  
+  console.log('purposefully to fail the test');
+  let x = 75;
 
   res.send('Works on my machine.');
   // res.send('Hello World!');   // khali fail feel karva karyu.
