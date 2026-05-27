b631b92ffe2546cd6a6841871f5785df4f778aa1
Sahil Bhanderi <sahilbhanderi02@gmail.com>
2026-05-27 18:20:18 +0530

    linting error fixed

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('purposefully to fail the test');
-  let x = 75;
+  // console.log('purposefully to fail the test');
+  // const x = 75;
 
   res.send('Works on my machine.');
   // res.send('Hello World!');   // khali fail feel karva karyu.
