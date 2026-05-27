6766fc8ac51eb9137ee981a8fda20da6a8ed056f
Sahil Bhanderi <sahilbhanderi02@gmail.com>
2026-05-27 14:11:00 +0530

    purposefully bad styling to see that it fails

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  // res.send('Works on my machine.');
-  res.send('Hello World!');   // khali fail feel karva karyu.
+app.get("/", (req, res) => {
+  res.send(         "Works on my machine.");
+  // res.send('Hello World!');   // khali fail feel karva karyu.
 });
 
 module.exports = app;
