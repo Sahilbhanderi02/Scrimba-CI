3332834f754e7f297bd83a715e5a92fe0d1dd0e8
Sahil Bhanderi <sahilbhanderi02@gmail.com>
2026-05-27 18:07:04 +0530

    purposefully failed the linting

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  
+  console.log('purposefully to fail the test')
+  let x= 75;
+  
+
   res.send('Works on my machine.');
   // res.send('Hello World!');   // khali fail feel karva karyu.
 });
